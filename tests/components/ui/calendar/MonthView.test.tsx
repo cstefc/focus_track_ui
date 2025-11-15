90c0b09629225d8a6f1ee7c9d6a925835bb09dff
Stef Ossé <stef.osse@ugent.be>
2025-11-15 01:32:19 +0100

    month-view tests

--- tests/components/ui/calendar/MonthView.test.tsx
+++ tests/components/ui/calendar/MonthView.test.tsx
@@ -1,10 +1,31 @@
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import MonthView from "../../../../src/components/ui/calendar/MonthView";
 
 describe("MonthView", () => {
     test("renders correctly", () => {
-        let date = new Date();
+        // GIVEN saturday 15/11/2025
+        let date = new Date(2025, 10, 15);
 
+        // WHEN
+        render(<MonthView selected={date} setSelected={newDate => date = newDate}/>);
+        screen.debug();
+
+        // THEN
+        const buttons = screen.getAllByRole("button");
+        expect(buttons.length % 7).toBe(0);
+    })
+
+    test("updates when clicked", async () => {
+        // GIVEN saturday 15/11/2025
+        let date = new Date(2025, 10, 15);
+
+        // WHEN
         render(<MonthView selected={date} setSelected={newDate => date = newDate}/>)
+        screen.debug();
+        screen.getByText("25").click();
+
+        // THEN
+        expect(date.getDate()).toBe(25);
+
     })
 })
