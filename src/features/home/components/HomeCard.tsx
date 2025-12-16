23777c5977f32c4ce4de8f6058995054306af791
Stef Ossé <stef.osse@ugent.be>
2025-12-16 18:50:25 +0100

    Improved home cards view

--- src/features/home/components/HomeCard.tsx
+++ src/features/home/components/HomeCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {useTranslation} from "react-i18next";
 
-import {Typography} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 
@@ -13,10 +13,12 @@ export default function HomeCard({card}: HomeCardProps) {
     const {t} = useTranslation("home");
 
     return (
-        <Card sx={{height: "300px", width: "300px", margin:"50px"}}>
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">{t(`cards.${card}.title`)}</Typography>
-                <Typography variant="body1" component="div">{t(`cards.${card}.text`)}</Typography>
+        <Card sx={{height: "320px", width: "320px", margin: "50px"}}>
+            <CardContent component={Box} height={"100%"} display="flex" flexDirection={"column"} justifyContent="space-around">
+                    <Typography gutterBottom variant="h5" component="div">{t(`cards.${card}.title`)}</Typography>
+                    <Box display={"flex"} alignItems={"center"} justifyContent={"center"}>
+                        <Typography variant="body1">{t(`cards.${card}.text`)}</Typography>
+                    </Box>
             </CardContent>
         </Card>
     );
