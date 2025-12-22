625496b0e28a973b8334e484608060c9f5cf21ec
Stef Ossé <stef.osse@ugent.be>
2025-12-22 17:09:55 +0100

    useGoals tested

--- tests/hooks/useGetApi.test.tsx
+++ tests/hooks/useGetApi.test.tsx
@@ -1,4 +1,4 @@
-import {Mock, vi} from "vitest";
+import {vi} from "vitest";
 import {renderHook, waitFor} from "@testing-library/react";
 import {useGetApi} from "../../src/hooks/useGetApi";
 import {getApi} from "../../src/api/apiCall";
@@ -7,12 +7,13 @@ vi.mock("@/api/apiCall", () => ({
     getApi: vi.fn(),
 }));
 
+const mockedGetApi = vi.mocked(getApi);
+
 describe("useGetApi", async () => {
     it("should call getApi correctly", async () => {
-
         // GIVEN
         const mockData = {value: "test value"};
-        (getApi<{ value: string }> as Mock).mockResolvedValue(mockData);
+        mockedGetApi.mockResolvedValue(mockData);
 
         // WHEN
         const {result} = renderHook(() => useGetApi("/test"));
@@ -21,14 +22,32 @@ describe("useGetApi", async () => {
         expect(result.current.loading).toBe(true);
         expect(result.current.data).toBeUndefined();
 
+        expect(getApi).toHaveBeenCalledWith("/test");
+        expect(getApi).toHaveBeenCalledTimes(1);
+
         // THEN after async API resolves
         await waitFor(() => {
             expect(result.current.loading).toBe(false);
+            expect(result.current.data).toEqual(mockData);
         })
-        expect(result.current.data).toEqual(mockData);
-
-        // API was called
-        expect(getApi).toHaveBeenCalledWith("/test");
-        expect(getApi).toHaveBeenCalledTimes(1);
     });
+
+    it("should handle null response correctly", async () => {
+        // GIVEN
+        const mockData = null;
+        mockedGetApi.mockResolvedValue(mockData);
+
+        // WHEN
+        const {result} = renderHook(() => useGetApi("/test"));
+
+        // THEN before await
+        expect(result.current.loading).toBe(true);
+
+        // THEN after await
+        await waitFor(() => {
+            expect(result.current.loading).toBe(false);
+            expect(result.current.data).toBeNull();
+        })
+
+    })
 })
